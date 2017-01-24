3126d6fac442361bc3249dce6c2e88b19df2383e
Lee Jacobson <lee.jacobson@cpap.com>
2017-01-24 16:48:40 +0000

    Added Github link

--- src/app/js/components/pages/About.js
+++ src/app/js/components/pages/About.js
@@ -6,7 +6,7 @@ export default class About extends React.Component {
 			<div className="pad">
 			<h1>About</h1>
 			<p>This demo React/Dedux app was built by <a href="http://www.leejacobson.co.uk/" target="_black">Lee Jacobson</a> using <a href="https://www.themoviedb.org/documentation/api" target="_blank">TMDB's API</a>.</p>
-			<p>GitHub: <br />
+			<p>GitHub: <a href="https://github.com/leejacobson/React-Redux-Demo/">https://github.com/leejacobson/React-Redux-Demo/</a><br />
 			Contact: lee [at] leejacobson.co.uk</p>
 			</div>
 		);
